faef549d971ff7380dbd7d3eb0ce983782aa1cb7
MikeTheFoxFromDream <KawaiiBoyKatsuky@gmail.com>
2022-05-24 14:11:29 +0200

    pj_bot

---
+++ BotTest_486/Commands.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTest_486
+{
+    class Commands
+    {
+
+        public static string Roll(string message)
+        {
+            string result = "";
+            var rn = new Random();
+            message = message.Replace("!roll ".ToLower(), "");
+            string text = "";
+            int numDice = 0;
+            int numSide = 0;
+            try
+            {
+                foreach (char i in message)
+                {
+                    if (i == 'd')
+                        break;
+                    else
+                    {
+                        text += i;
+                    }   
+                }
+
+                numDice = int.Parse(text);
+                if (numDice > 125)
+                    return "Kámo tolik kostek tu nemám";
+                text += 'd';
+                message = message.Replace(text, "");
+                text = "";
+
+
+                foreach (char i in message)
+                {
+                    if (i == ' ')
+                        break;
+                    else
+                    {
+                        text += i;
+                    }
+                }
+
+                numSide = int.Parse(text);
+                if (numSide > 100)
+                    return "Tak takový kostky mi neprodá ani Pepa, a to je co říct";
+
+                while (numDice != 0)
+                {
+                    result += rn.Next(1, numSide + 1) + ", ";
+                    numDice--;
+                }
+
+                result = result.Remove(result.Length - 2);
+                return result;
+            }
+            catch
+            {
+                return("Co to meleš!");
+            }
+        }
+    }
+}
---
+++ BotTest_486/Program.cs
@@ -0,0 +1,36 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+
+namespace BotTest_486
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "<token>",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("!hello"))
+                    await e.Message.RespondAsync("Hello World!");
+                if (e.Message.Content.ToLower().StartsWith("!roll"))
+                    await e.Message.RespondAsync(Commands.Roll(e.Message.Content));
+                
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
